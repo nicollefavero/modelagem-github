6fb3ab5904438c9712c7aec68f55be077cbfb35d
Matheus Azambuja <matheusazambuja_35@gmail.com>
2020-10-02 02:39:46 -0300

    90% tables created (falta relacao commit)

--- 02 - Projeto Logico/gitHub.sql
+++ 02 - Projeto Logico/gitHub.sql
@@ -4,7 +4,8 @@ create table Users
  nickname   varchar(30)  notnull,
  biografia  varchar(160) notnull,
  email      varchar(50)  notnull,
- primary key(id));
+ primary key(id)
+ unique(nickname)); -- NICKNAME É UNICO??
 
 create table Contributors
 (id         integer      notnull,
@@ -42,10 +43,41 @@ create table Repositories
  foreign key(idUser) references Users,
  unique(idUser, name));
 
--- create table License
--- (id          integer       notnull,
---  type        varchar(60)   notnull,
---  primary key(id));
+create table Stars
+(idContrib    integer notnull,
+ idRepository integer notnull,
+ date         Date    notnull,
+ primary key(idContrib, idRepository),
+ foreign key(idContrib) references Contributors,
+ foreign key(idRepository) references Repositories);
+
+-- LICENSES É ATRIB-MULTVALOR?? OU N-M TALVEZ??
+-- create table Licenses
+-- (cod          integer     notnull,
+--  idRepository integer     notnull,
+--  type         varchar(60) notnull,
+--  primary key(cod, idRepository),
+--  foreign key(idRpository) references Repositories);
+
+create table Issues
+(number       integer       notnull,
+ description  varchar(2500) notnull,
+ date         Date          notnull,
+ idRepository integer       notnull,
+ idContrib    integer       notnull,
+ primary key(number, idRepository),
+ foreign key(idRepository) references Repositories,
+ foreign key(idContrib) references Contributors);
+
+-- COMENTAR SOBRE PRECISAR DE DESCRIPTION EM COMMENT
+-- create table Comment
+-- (idContrib   integer notnull,
+--  numIssue    integer notnull,
+--  date        Date    notnull,
+--  description integer nontnull,
+--  primary key(idContrib, numIssue, date),
+--  foreign key(idContrib) references Contributors,
+--  foreign key(numIssue) references Issues);
 
 create table Topics
 (cod     integer     notnull,
@@ -67,7 +99,18 @@ create table Items
  primary key(id),
  foreign key(idRepository) references Repositories)
 
--- create table Folders()
+create table Folders
+(id         integer notnull,
+ idItem     integer notnull,
+ primary key(id),
+ foreign key(idItem) references Items);
+
+create table FullWith
+(idItem integer notnull,
+ idFolder integer notnull,
+ primary key(idItem),
+ foreign key(idItem) references Items,
+ foreign key(idFolder) references Folders);
 
 create table Files
 (id          integer    notnull,
@@ -87,3 +130,5 @@ create table Implementations
  primary key(idFile),
  foreign key(idFile) references Files,
  foreign key(codLanguage) references Languages);
+
+-- create table Commits()
