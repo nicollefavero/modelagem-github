8663441b2f1b8e64ed2695eb95a768c4adbeed7a
Nicolle <nicollepf@gmail.com>
2020-10-03 17:57:37 -0300

    adicionando finalizações

--- 02 - Projeto Logico/gitHub.sql
+++ 02 - Projeto Logico/gitHub.sql
@@ -14,8 +14,21 @@ insert into Users values (3, null, 'nicollefavero', null, 'nfavero@inf.br');
 insert into Users values (4, 'Netflix, Inc.', 'Netflix', 'Netflix Open Source Platform', 'netflixoss@netflix.com');
 insert into Users values (5, 'Google', 'google', 'Google ❤️ Open Source', 'opensource@google.com');
 insert into Users values (6, 'João da Silva', 'jsilva', null, 'jsilva35@gmail.com');
+insert into Users values (7, 'Facebook', 'facebook', 'Facebook', 'facebook@facebook.com');
 
-select * from Users;
+-- inserindo elemento repetido com id repetido
+-- insert into Users values (4, 'Netflix', 'Netflix', 'Netflix Open Source Platform', 'netflixoss@netflix.com');
+
+-- inserindo elemento com nome repetido
+-- insert into Users values (8, 'Netflix, Inc.', 'Netflix', 'Netflix Open Source Platform', 'netflixoss@netflix.com');
+
+-- inserindo elemento com nickname null
+-- insert into Users values (9, 'Gradle' , null, 'Dependency Manager', 'gradle@email.com');
+
+-- inserindo elemento com email null
+-- insert into Users values (9, 'Gradle' , 'gradle', 'Dependency Manager', null);
+
+select * from Users; 
 
 ------------ CONTRIBUIDORES ---------------------------------------------------------------------------
 create table Contributors
@@ -28,6 +41,12 @@ insert into Contributors values (2);
 insert into Contributors values (3);
 insert into Contributors values (6);
 
+-- inserindo de contributor já cadastrado
+-- insert into Contributors values (1);
+
+-- atualizando chave estrangeira
+-- update Users set id = 9 where id = 3;
+
 select * from Contributors
 
 ------------ ORGANIZAÇÕES -------------------------------------------------------------------------------
@@ -38,6 +57,13 @@ create table Organizations
 
 insert into Organizations values (4);
 insert into Organizations values (5);
+insert into Organizations values (7);
+
+-- inserindo organization já cadastrado
+-- insert into Organizations values (4);
+
+-- atualizando chave estrangeira
+-- update Users set id = 8 where id = 4;
 
 select * from Organizations;
 
@@ -55,6 +81,9 @@ insert into Members values (2, 4);
 insert into Members values (2, 5);
 insert into Members values (3, 4);
 
+-- inserindo par (contrib, org) já cadastrado
+-- insert into Members values (1, 4)
+
 select * from Members;
 
 ------------ FOLLOWS -------------------------------------------------------------------------------
@@ -70,6 +99,9 @@ insert into Follows values (1, 2, '2020-10-02 00:00:00 UTC');
 insert into Follows values (2, 3, '2020-10-02 00:00:00 UTC');
 insert into Follows values (2, 1, '2020-10-02 00:00:00 UTC');
 insert into Follows values (6, 2, '2020-10-02 00:00:00 UTC');
+
+-- inserindo par (follower, followed) já cadastrado
+-- insert into Follows values (1, 2, '2019-10-02 09:12:10 UTC');
 
 select * from Follows;
 
@@ -87,6 +119,12 @@ insert into Repositories values (1, 1, 'computer-science-college', '2018-08-08 0
 insert into Repositories values (2, 2, 'repo-da-karin', '2020-01-01 00:00:00 UTC');
 insert into Repositories values (3, 3, 'superjava-advanced', '2019-11-04 00:00:00 UTC');
 
+-- inserindo par (idUser, name) ja cadastrado
+-- insert into Repositories values (4, 1, 'computer-science-college', '2020-08-08 00:00:00 UTC');
+
+-- alterando id do usuário (chave estrangeira)
+-- update Users set id = 9 where id = 2;
+
 select * from Repositories;
 
 ------------ STARS -------------------------------------------------------------------------------
@@ -99,6 +137,16 @@ create table Stars
  foreign key(idRepo) references Repositories) on delete cascade;
 
 insert into Stars values (1, 2, '2020-01-02 00:00:00 UTC');
+insert into Stars values (2, 3, '2019-09-10 12:01:59 UTC');
+insert into Stars values (1, 3, '2019-09-10 12:01:59 UTC');
+insert into Stars values (3, 2, '2019-09-10 12:01:59 UTC');
+
+-- inserindo par (idContrib, idRepo) já existe
+-- insert into Stars values (1, 3, '2020-05-10 12:01:59 UTC');
+
+-- alterando id de um repositório
+-- update Repositories set idRepo = 4 where idRepo = 3;
+
 
 select * from Stars;
 
@@ -110,12 +158,16 @@ select * from Stars;
 
 insert into Licenses values (1, 'GNU-3');
 insert into Licenses values (2, 'GNU-2');
+insert into Licenses values (3, 'MIT Licenses');
+
+-- inserindo licensa já existente
+-- insert into Licenses values (1, 'No-License');
 
 select * from Licenses;
 
 ------------ ISSUES -------------------------------------------------------------------------------
  create table Issues
-(idIssue      varchar(5)      not null,
+(idIssue      integer      not null,
  description  varchar(2500) not null,
  date         timestamp     not null,
  idRepo       integer       not null,
@@ -124,8 +176,16 @@ select * from Licenses;
  foreign key(idRepo) references Repositories,
  foreign key(idContrib) references Contributors);
 
-insert into Issues values ('#1', 'Não está funcionando', '2020-01-01 00:00:00 UTC', 1, 3);
-insert into Issues values ('#1', 'Sabe nada de Java', '2020-01-01 00:00:00 UTC', 3, 1);
+insert into Issues values (1, 'Não está funcionando', '2020-01-01 00:00:00 UTC', 1, 3);
+insert into Issues values (2, 'Sabe nada de Java', '2020-01-01 00:00:00 UTC', 3, 1);
+insert into Issues values (2, 'Sabe nada de Java', '2020-01-01 00:00:00 UTC', 2, 1);
+insert into Issues values (3, 'Arquivo aulaEspecializacao não está abrindo', '2020-01-01 00:00:00 UTC', 3, 1);
+
+-- inserindo par (idIssue, idRepo) ja existe
+-- insert into Issues values (3, 'Arquivo aulaEspecializacao não está abrindo', '2020-01-01 00:00:00 UTC', 3, 3);
+
+-- alterando id de um repositório
+-- update Repositories set idRepo = 4 where idRepo = 3;
 
 select * from Issues;
 
@@ -137,12 +197,25 @@ idIssue    integer not null,
 date       timestamp not null,
 description varchar(2500) not null,
 primary key(idContrib, idIssue, idRepo, date),
-foreign key(idContrib) references Contributors,
-foreign key (idRepo) references Repositories,
-foreign key(idIssue) references Issues);
+foreign key(idContrib) references Contributors on delete set null,
+foreign key (idRepo) references Repositories on delete cascade,
+foreign key(idIssue) references Issues on delete cascade);
 
 insert into Comments values (3, 3, 1, '2020-01-01 00:00:00 UTC', 'Sei sim!');
 insert into Comments values (2, 1, 1, '2020-01-01 00:00:00 UTC', 'Muito bom! Parabééns!');
+insert into Comments values (6, 2, 2, '2020-01-01 00:00:00 UTC', 'Muito bom! Parabééns!');
+
+-- (idContrib, idIssue, idRepo, date) já existe
+-- insert into Comments values (6, 2, 2, '2020-01-01 00:00:00 UTC', 'Muito bom! Parabééns!');
+
+-- alterando id de um repositório
+-- update Repositories set idRepo = 4 where idRepo = 3;
+
+-- alterando id de um Issue
+-- update Issues set idIssue = 4 where idIssue = 1;
+
+-- deletando um contribuidor
+-- delete * from Contributor where id = 2;
 
 select * from Comments;
 
@@ -155,6 +228,12 @@ create table Topics
 
 insert into Topics values (1, 'sgbds-fbd');
 insert into Topics values (2, 'ufrgs');
+insert into Topics values (3, 'python');
+insert into Topics values (4, 'java');
+insert into Topics values (5, 'fbd');
+
+-- inserindo id ja existente
+-- insert into Topics values (4, 'c-like');
 
 select * from Topics;
 
@@ -168,6 +247,14 @@ select * from Topics;
 
 insert into Categories values (2, 1);
 insert into Categories values (1, 2);
+insert into Categories values (3, 5);
+
+-- inserindo par (idRepo, codTopic) ja existe
+-- insert into Categories values (3, 5);
+
+-- alterando id de um repositório
+-- update Repositories set idRepo = 4 where idRepo = 3;
+
 
 select * from Categories;
 
@@ -187,6 +274,9 @@ insert into Items values (5, 'Python', 1);
 insert into Items values (6, 'Java', 2);
 insert into Items values (7, 'Aula', 3);
 
+-- idItem já existe
+-- insert into Items values (7, 'FBD', 3);
+
 select * from Items;
 
 ------------ FOLDERS -------------------------------------------------------------------------------
@@ -200,6 +290,9 @@ insert into Folders values (1, 4); -- Item botDiscord
 insert into Folders values (2, 5); -- Item Python
 insert into Folders values (3, 6); -- Item Java
 insert into Folders values (4, 7); -- Item Aula
+
+-- idFolder já existe
+-- insert into Folders values (4, 3);
 
 select * from Folders;
 
@@ -219,6 +312,9 @@ insert into Saves values (5, null); -- Item Python nao associado a nenhuma pasta
 insert into Saves values (6, null); -- Item Java nao associado a nenhuma pasta
 insert into Saves values (7, null); -- Item Aula nao associado a nenhuma pasta
 
+-- idItem ja existe
+-- insert into Saves values (7, null);
+
 select * from Saves;
 
 ------------ FILES -------------------------------------------------------------------------------
@@ -232,6 +328,9 @@ create table Files
 insert into Files values (1, 1, '.py');
 insert into Files values (2, 2, '.ppt');
 insert into Files values (3, 3, '.java');
+
+-- idItem ja existe
+-- insert into Files values (3, 3, '.java');
 
 select * from Files;
 
@@ -249,6 +348,9 @@ insert into Languages values (5, 'JavaScript');
 insert into Languages values (6, 'Assembly');
 insert into Languages values (7, 'SQL');
 
+-- codLang ja existe
+-- insert into Languages values (7, 'SQL');
+
 select * from Languages;
 
 ------------ IMPLEMENTATIONS -------------------------------------------------------------------------------
@@ -262,6 +364,9 @@ create table Implementations
 insert into Implementations values (1, 1);
 insert into Implementations values (2, null);
 insert into Implementations values (3, 2);
+
+-- idFile ja existe
+-- insert into Implementations values (1, null);
 
 select * from Implementations;
 
@@ -283,5 +388,11 @@ create table Commits
  insert into Commits values (2, 1, 2, 3, '2020-10-15 10:32:19 UTC', 'Mensagem 2');
  insert into Commits values (3, 3, 2, 1, '2019-02-12 01:12:32 UTC', 'Mensagem 3');
  insert into Commits values (4, 1, 3, 1, '2020-10-15 10:32:19 UTC', 'Mensagem 4');
+
+ -- hashCode ja existe
+ -- insert into Commits values (1, 2, 2, 1, '2020-12-12 01:12:32 UTC', 'Mensagem 10');
+
+ -- (idItem, idRepo, idContrib, date) ja existe
+ -- insert into Commits values (5, 1, 3, 1, '2020-10-15 10:32:19 UTC', 'Mensagem 9999');
 
 select * from Commits;
--- README.md
+++ README.md
@@ -8,25 +8,10 @@ Modelar o universo de discurso do GitHub e, a partir da modelagem, implementar o
 
 ### Roadmap
 
-- [X] Ligar a entidade REPOSITORY à LANGUAGES?
-- [X] FILES.Tipo pode ser .sql, .json, .py ou a palavra "diretório"?
-- [X] Colocar TIPO (de FILES) uma entidade pré-definida?
-- [ ] Falta colocar a cardinalidade em REPOSITORY -> LICENSE.
-- [ ] Ver as coisas do marketplace e colocar a parte dos APPS em ORGS.
-- [ ] Revisar os nomes dos relacionamentos.
-- [ ] Acrescentar papéis.
-- [ ] Trocar o relacionamento REPOSITORY -> CONTEM -> FILES para REPOSITORY -> COMMIT -> FILES??
-- [X] O que é TOPICS IN REPOSITORY?
-
-Com base na descrição do UdD, analisar:
-- [ ] Relacionamento Possui (User/Repository): CreationDate deve migrar para Repository?
-- [ ] Relacionamento Fork: O relacionamento Contribui já não abstrai o conceito do Fork?
-- [ ] Relacionamento Follow: Ele representa uma ação realizada pelo Contributor. Auto-relacionamento.
-- [ ] Nossa intenção é representar um "print" do repositório ou alguma hierarquia, juntamente com a representação de Branch?
-- [ ] Com base do questionamento de cima, o relacionamento de Contribui e Issues são feitos no repositório e ou em arquivos do repositório?
-- [ ] Relacionamento 'Star' -> (0,1) ?
+- [X] Modelagem Conceitual
+- [X] Projeto Lógico
+- [ ] Correção da Modelagem Conceitual
+- [ ] Correção do Projeto Lógico
+- [ ] Projeto Físico
 
 
-### Perguntar para a professora
-- [ ] REPOSITORY -> create -> USER???
-- [ ] Files/Folders -> Languages/Type???
